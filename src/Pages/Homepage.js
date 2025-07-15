525a4299b9d79dbdda79ba5c593ac9d55bad4c70
kaushlendra <singhkaushlendra5002@gmail.com>
2025-07-15 18:22:48 +0530

    added achievement section

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -17,8 +17,11 @@ import Footer from "../components/Footer";
 import TestimonialHomePage from "../components/Testimonial-HomePage";
 import Environment from "../components/environment";
 import EnvironmentSection1 from "../components/EnvironmentSection1";
+
 import StatsCard from "../components/StatsCard.jsx";
 import BusinessHomepage from "../components/BusinessHomepage";
+import AchievementSection from "../components/Achievement.js";
+
 import Hero from "../components/hero2";
 import BusinessVerticals from "../components/BusinessVerticals.js";
 // import BusinessVerticals from "../components/BusinessVerticals.js";
@@ -36,7 +39,9 @@ const HomePage = () => {
       <BusinessAreas id="business-areas" />
       <LocationsMap />
       <EnvironmentSection1 />
+    
       <CSRPage />
+      <AchievementSection />
       <TestimonialHomePage />
       <CertificationsSection />
       <NewsPage />
